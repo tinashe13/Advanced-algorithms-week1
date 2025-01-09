68200cc6d16820f5bf8f2299ec4a52859394f7c6
Tinashe <tinashedzemwa300@gmail.com>
2025-01-08 23:09:05 -0600

    solution

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Advanced-algorithms-week1
+Description and understanding
+
+I have come across this problem on leetcode platform, and it was my first problem solved on the platform and it introduced me to the wonderful world of algorthms and built my intrest to problemsolving
+
+
+Solution 1
+
+* This solution involves using a nested for loop to go through the array and find every combination to check if the combination equals to out target
+* This solution will give us a correct answer but it is not time efficient, it runs ata worst case time complexity of O(n^2) where n is the size of the array
+
+Solution 2
+
+* Solution 2 involved the use of an aditional data structure in the for of an unordered map (Hashmap) to store the value of the indicies we have seen as a key value pai of value and index
+* This approach reduced the lookup time to O(n) where n is the size of the array, but it is important to note that by introducing a hashmap we will now be sacrificing on space efficiency and now our space ccplexity becaomes O(n) instead of O(1)
--- main.cpp
+++ main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+
+// Solution 1
+// Finding the two indexes using nested for loop
+vector<int> TwoSum1(vector<int> nums, int target) {
+    for (int i = 0; i < nums.size(); i++) {
+        for (int j = i + 1; j < nums.size(); j++) {
+            if (nums[i] + nums[j] == target) {
+                // Return 0 based indices
+                return {i , j};
+            }
+        }
+    }
+    // No solution found
+    return {-1, -1};
+}
+
+// solution 2
+// Using a hashmap to store values we have seen before
+vector<int> TwoSum2(vector<int> nums, int target) {
+    // Stores number -> index
+    unordered_map<int, int> memory;
+
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        if (memory.find(complement) != memory.end()) {
+            // Return 0 based indices
+            return {memory[complement], i};
+        }
+        // Store the current number with its index
+        memory[nums[i]] = i;
+    }
+    // No solution found
+    return {-1, -1};
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<int> result1 = TwoSum1({2,7,11,15}, 9);
+    vector<int> result2 = TwoSum2({3,2, 4}, 6);
+
+    cout << "TwoSum1 gives: " << result1[0] << ", " << result1[1] << endl;
+    cout << "TwoSum2 gives: " << result2[0] << ", " << result2[1] << endl;
+
     return 0;
 }
